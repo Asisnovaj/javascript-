f1db87abbc592d17a9bb32eaa9cf3f26304ec935
Asisnovaj <ananciahalee3@gmail.com>
2023-09-07 20:19:22 +0530

    basic of js

---
+++ Day1/msg.js
@@ -0,0 +1,20 @@
+function dis() {
+    document.getElementById("p1").innerHTML = "Name- ASIS NOVA J"
+    document.getElementById("p1").style.fontSize = "20px"
+    document.getElementById("p1").style.fontFamily = "times new romen"
+    document.getElementById("p1").style.background = "green"
+    document.getElementById("p1").style.color = "red"
+
+    document.getElementById("p2").innerHTML = " Age-21"
+    document.getElementById("p2").style.fontSize = "20px"
+    document.getElementById("p2").style.fontFamily = "times new romen"
+    document.getElementById("p2").style.background = "red"
+    document.getElementById("p2").style.color = "green"
+
+    document.getElementById("p3").innerHTML = " BE.ECE"
+    document.getElementById("p3").style.fontSize = "20px"
+    document.getElementById("p3").style.fontFamily = "times new romen"
+    document.getElementById("p3").style.background = "blue"
+    document.getElementById("p3").style.color = "grey"
+
+}
