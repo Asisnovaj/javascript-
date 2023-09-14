cfb065f64ba4919e3f576c753479175541faeb02
Asisnovaj <ananciahalee3@gmail.com>
2023-09-14 20:49:03 +0530

    variable task 3

---
+++ Day2/tasks and assignment/variable task 2.js
@@ -0,0 +1,4 @@
+var fristname = "Ram", lastname = "kumar", age = "30";
+document.write("fristname is" + fristname + "<br>");
+document.write("lastname is" + lastname + "<br>");
+document.write("age is" + age + "<br>");
