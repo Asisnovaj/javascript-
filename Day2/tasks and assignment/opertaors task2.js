ec676ab9d9c9641c8e2da6780ad0c14b142ffb53
Asisnovaj <ananciahalee3@gmail.com>
2023-09-15 20:29:25 +0530

    module task

---
+++ Day2/tasks and assignment/opertaors task2.js
@@ -0,0 +1,9 @@
+var anchoviesPizzaScore = 0;
+var pepporoniPizzaScore = 4;
+var hawaiianPizzaScore = 4;
+var chickenBaconRanchPizzaScore = 6;
+document.write(anchoviesPizzaScore < pepporoniPizzaScore);
+document.write(chickenBaconRanchPizzaScore > hawaiianPizzaScore);
+document.write(hawaiianPizzaScore > pepporoniPizzaScore);
+document.write(pepporoniPizzaScore <= chickenBaconRanchPizzaScore);
+document.write(hawaiianPizzaScore >= pepporoniPizzaScore);
--- Day2/tasks and assignment/variable task3.js
+++ Day2/tasks and assignment/variable task3.js
@@ -1,6 +1,6 @@
 <script>
     funcation DOB(){
-    var bday=2001,currentyear=2023,age,or;
+        varbday = 2001, currentyear = 2023, age, or;
     var age=currentyear-bday;
     or=age-1;
     document.write(age);
