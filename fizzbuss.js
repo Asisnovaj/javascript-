d321c1bc6e2e6faff6478f86ce380204c677074a
Asisnovaj <ananciahalee3@gmail.com>
2023-09-16 16:45:33 +0530

    fizzbuss

---
+++ fizzbuss.js
@@ -0,0 +1,14 @@
+for (i = 0; i <= 100; i++) {
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        document.writeln(i + "fizzbuzz");
+    }
+    else if (i % 3 == 0) {
+        document.writeln(i + "fizz");
+    }
+    else if (i % 5 == 0) {
+        document.writeln(i + "buzz");
+    }
+    else {
+        document.writeln(i);
+    }
+}
